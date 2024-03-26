a632a66baf9c3720c7418d80a0ee4362fc2df683
TE22B-Johansson-Max <max.johansson2@elev.ga.ntig.se>
2024-03-26 11:10:17 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+/* *************************************************
+ * Namn: CitatGenerator
+ * Beskrivning: Ett program för att slumpa fram kända citat
+ * Av: Max
+ *********************************************** */
+
+Console.Clear();
+
+string filnamn = "quotes.txt";
+
+
+// Programloop
+    // Meny
+
+    // Switch-sats
+
+/* ************************************************
+ * **************** Metoder ***********************
+ ************************************************* */
+
+static void SlumpaCitat(string filnamn = "quotes.txt") {
+    string[] citat = File.ReadAllLines(filnamn);
+    if (citat == null) {
+        Console.WriteLine(citat);
+    }
+    
+
+ }
+
+
+
+ /* Karims Svar */
+
+List<string> rader = File.ReadAllLines("quotes.txt").ToList<string>();
+List<string> citatLista = new List<string>();
+
+string citat = "";
+
+foreach (var rad in rader)
+{
+
+    if (rad != null)
+    {
+        citat += rad;
+    }
+    else
+    {
+        citatLista.Add(citat);
+        citat = "";
+    }
+}
+
+ Console.ReadLine();
